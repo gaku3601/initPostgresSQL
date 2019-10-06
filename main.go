17f47b16ec0dc8e45055f5fa0c9d8936079f2b44
日高岳大 <gaku@gakumbp.local>
2019-10-06 15:30:41 +0900

    sqlファイル一覧を取得するように実装

--- main.go
+++ main.go
@@ -1,32 +1,26 @@
 package main
 
 import (
-	"strings"
-
-	"github.com/lxn/walk"
-	. "github.com/lxn/walk/declarative"
+	"flag"
+	"fmt"
 )
 
 func main() {
-	var inTE, outTE *walk.TextEdit
+	// 左から順にオプション名、デフォルトの値、helpテキストが引数に入る
+	sqlFolder := flag.String("s", "", "SqlFolder Path")
+	host := flag.String("h", "", "host名")
+	port := flag.Int("p", 0, "port番号")
+	dbName := flag.String("d", "", "database名")
+	userName := flag.String("u", "", "user name")
+	password := flag.String("pw", "", "password")
+	flag.Parse()
+	fmt.Println(*host)
+	fmt.Println(*port)
+	fmt.Println(*dbName)
+	fmt.Println(*sqlFolder)
+	fmt.Println(*userName)
+	fmt.Println(*password)
 
-	MainWindow{
-		Title:   "SCREAMO",
-		MinSize: Size{600, 400},
-		Layout:  VBox{},
-		Children: []Widget{
-			HSplitter{
-				Children: []Widget{
-					TextEdit{AssignTo: &inTE},
-					TextEdit{AssignTo: &outTE, ReadOnly: true},
-				},
-			},
-			PushButton{
-				Text: "SCREAM",
-				OnClicked: func() {
-					outTE.SetText(strings.ToUpper(inTE.Text()))
-				},
-			},
-		},
-	}.Run()
+	o := NewOperateFile(*sqlFolder)
+	fmt.Print(o)
 }
---
+++ testSql/test2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Staff2
+(id    CHAR(4)    NOT NULL,
+name   TEXT       NOT NULL,
+age    INTEGER    ,
+PRIMARY KEY (id));
